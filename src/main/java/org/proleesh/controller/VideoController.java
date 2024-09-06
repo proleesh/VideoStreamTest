36f5a1bf4c98e4a2d6194a5467ddfe6eeedeaa6b
proleesh <merci726@yahoo.com>
2024-09-07 06:24:56 +0900

    update about Rest API logic

--- src/main/java/org/proleesh/controller/VideoController.java
+++ src/main/java/org/proleesh/controller/VideoController.java
@@ -1,9 +1,11 @@
 package org.proleesh.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.proleesh.entity.Video;
 import org.proleesh.playload.CustomMessage;
 import org.proleesh.services.VideoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,7 @@ public class VideoController {
 
     private final VideoService videoService;
     @PostMapping
-    public ResponseEntity<CustomMessage> create(
+    public ResponseEntity<?> create(
             @RequestParam("file") MultipartFile file,
             @RequestParam("title") String title,
             @RequestParam("description") String description
@@ -29,7 +31,17 @@ public class VideoController {
         video.setTitle(title);
         video.setDescription(description);
         video.setVideoId(UUID.randomUUID().toString());
-        videoService.save(video, file);
-        return null;
+        Video savedVideo = videoService.save(video, file);
+
+        if(savedVideo != null){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(video);
+        }else{
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(CustomMessage.builder()
+                            .message("동영상 업로드 안함")
+                            .success(false)
+                            .build());
+        }
     }
 }
--- src/main/java/org/proleesh/services/impl/VideoServiceImpl.java
+++ src/main/java/org/proleesh/services/impl/VideoServiceImpl.java
@@ -1,27 +1,47 @@
 package org.proleesh.services.impl;
 
-import lombok.val;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.proleesh.entity.Video;
+import org.proleesh.repository.VideoRepository;
 import org.proleesh.services.VideoService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class VideoServiceImpl implements VideoService {
 
     @Value("${files.video}")
     String DIR;
 
+    private final VideoRepository videoRepository;;
+
+    @PostConstruct
+    public void init(){
+        File file = new File(DIR);
+
+        if(!file.exists()){
+            boolean mkdir = file.mkdir();
+            System.out.println("풀더 생성: " + mkdir);
+        }else{
+            System.out.println("풀더 이미 생성");
+        }
+    }
+
     @Override
-    public Video save(Video vide, MultipartFile file) {
+    public Video save(Video video, MultipartFile file) {
 
         try {
             // original file name
@@ -33,20 +53,27 @@ public class VideoServiceImpl implements VideoService {
             String cleanFileName = StringUtils.cleanPath(filename);
             String cleanFolder = StringUtils.cleanPath(DIR);
 
-            Path path = Paths.get(cleanFolder, cleanFileName);
-            System.out.println(path);
 
             // folder path with filename
+            Path path = Paths.get(cleanFolder, cleanFileName);
+            System.out.println(contentType);
+            System.out.println(path);
 
             // copy file to the folder
+            Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
+
 
             // video meta data
+            video.setContentType(contentType);
+            video.setFilePath(path.toString());
+
+            return videoRepository.save(video);
 
             // metadata save
         }catch(IOException e) {
-
+            e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
     @Override
