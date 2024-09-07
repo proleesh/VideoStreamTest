e67ce69d2a2b131ddb6c4be06defe699c139a811
proleesh <merci726@yahoo.com>
2024-09-07 16:57:34 +0900

    update about Cross Origin

--- src/main/java/org/proleesh/controller/VideoController.java
+++ src/main/java/org/proleesh/controller/VideoController.java
@@ -7,10 +7,7 @@ import org.proleesh.playload.CustomMessage;
 import org.proleesh.services.VideoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
@@ -18,6 +15,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/videos")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class VideoController {
 
     private final VideoService videoService;
